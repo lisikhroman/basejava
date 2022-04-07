b7eee7ae9fd30e4b9e502d3b924c2d6f779bd8d0
roman.lisikh@indusoft.ru <roman.lisikh@indusoft.ru>
2022-04-07 21:52:14 +0700

    Lesson05 Static initializer block and Collections

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -10,11 +10,25 @@ import ru.javawebinar.basejava.model.Resume;
 import static org.junit.Assert.*;
 
 public abstract class AbstractArrayStorageTest {
+    private final Storage storage;
+
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
     private static final String UUID_4 = "uuid4";
-    private final Storage storage;
+
+    private static final Resume RESUME_1;
+    private static final Resume RESUME_2;
+    private static final Resume RESUME_3;
+    private static final Resume RESUME_4;
+
+    static {
+        RESUME_1 = new Resume(UUID_1);
+        RESUME_2 = new Resume(UUID_2);
+        RESUME_3 = new Resume(UUID_3);
+        RESUME_4 = new Resume(UUID_4);
+    }
+
 
     public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
