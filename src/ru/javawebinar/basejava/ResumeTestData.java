1f4c729c81f69eeb3394eb58dd55a4df979c1955
roman.lisikh@indusoft.ru <roman.lisikh@indusoft.ru>
2022-06-23 15:43:47 +0700

    implement_HW_lesson7

--- src/ru/javawebinar/basejava/ResumeTestData.java
+++ src/ru/javawebinar/basejava/ResumeTestData.java
@@ -38,41 +38,37 @@ public class ResumeTestData {
                 "•\tJEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2;"
                 , "•\tVersion control: Subversion, Git, Mercury, ClearCase, Perforce"
         )));
-        resume_1.setSections(SectionType.EXPERIENCE,
-                new ListExperience(List.of(new Experience(
-                        "Enkata"
-                        , "http://enkata.com/"
-                        , LocalDate.of(2007, 3, 1)
-                        , LocalDate.of(2008, 6, 1)
-                        , "Разработчик ПО"
-                        , "Реализация клиентской (Eclipse RCP) и серверной (JBoss 4.2, Hibernate 3.0, " +
-                        "Tomcat, JMS) частей кластерного J2EE приложения (OLAP, Data mining)."))));
-        resume_1.setSections(SectionType.EXPERIENCE,
-                new ListExperience(List.of(new Experience(
-                        "Wrike"
-                        , "https://www.wrike.com/"
-                        , LocalDate.of(2014, 10, 1)
-                        , LocalDate.of(2016, 1, 1)
-                        , "Старший разработчик (backend)"
-                        , "Проектирование и разработка онлайн платформы управления проектами Wrike " +
-                        "(Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). " +
-                        "Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO."))));
-        resume_1.setSections(SectionType.EDUCATION,
-                new ListExperience(List.of(new Experience(
-                        "Luxoft"
-                        , "https://ibs-training.ru/kurs/obektno-orientirovannyy_analiz_i_proektirovanie_na_uml.html"
-                        , LocalDate.of(2011, 3, 1)
-                        , LocalDate.of(2011, 4, 1)
-                        , "Курс 'Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.'"
-                        , ""))));
-        resume_1.setSections(SectionType.EDUCATION,
-                new ListExperience(List.of(new Experience(
-                        "Coursera"
-                        , "https://www.coursera.org/course/progfun"
-                        , LocalDate.of(2013, 3, 1)
-                        , LocalDate.of(2013, 5, 1)
-                        , "'Functional Programming Principles in Scala' by Martin Odersky"
-                        , ""))));
+        resume_1.setSections(SectionType.EXPERIENCE, new ListExperience(List.of(new Experience(
+                "Enkata"
+                , "http://enkata.com/"
+                , LocalDate.of(2007, 3, 1)
+                , LocalDate.of(2008, 6, 1)
+                , "Разработчик ПО"
+                , "Реализация клиентской (Eclipse RCP) и серверной (JBoss 4.2, Hibernate 3.0, " +
+                "Tomcat, JMS) частей кластерного J2EE приложения (OLAP, Data mining)."))));
+        resume_1.setSections(SectionType.EXPERIENCE, new ListExperience(List.of(new Experience(
+                "Wrike"
+                , "https://www.wrike.com/"
+                , LocalDate.of(2014, 10, 1)
+                , LocalDate.of(2016, 1, 1)
+                , "Старший разработчик (backend)"
+                , "Проектирование и разработка онлайн платформы управления проектами Wrike " +
+                "(Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). " +
+                "Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO."))));
+        resume_1.setSections(SectionType.EDUCATION, new ListExperience(List.of(new Experience(
+                "Luxoft"
+                , "https://ibs-training.ru/kurs/obektno-orientirovannyy_analiz_i_proektirovanie_na_uml.html"
+                , LocalDate.of(2011, 3, 1)
+                , LocalDate.of(2011, 4, 1)
+                , "Курс 'Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.'"
+                , ""))));
+        resume_1.setSections(SectionType.EDUCATION, new ListExperience(List.of(new Experience(
+                "Coursera"
+                , "https://www.coursera.org/course/progfun"
+                , LocalDate.of(2013, 3, 1)
+                , LocalDate.of(2013, 5, 1)
+                , "'Functional Programming Principles in Scala' by Martin Odersky"
+                , ""))));
     }
 
     public static void main(String[] args) {
